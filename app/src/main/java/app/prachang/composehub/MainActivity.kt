908f44c7455dc9fc5f1d7cb3186ff8a3c0c20213
ghaleprachan <ghale.prachan@gmail.com>
2022-02-01 22:00:52 +0545

    [Add] adds insta profile

--- app/src/main/java/app/prachang/composehub/MainActivity.kt
+++ app/src/main/java/app/prachang/composehub/MainActivity.kt
@@ -1,15 +1,27 @@
 package app.prachang.composehub
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.prachang.dummy_data.instagram.profileData
 import app.prachang.theme.ComposeHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,11 +33,98 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        Text(text = "I'm Prachan Ghale")
-                    }
+                    ProfileScreen()
                 }
             }
         }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+internal fun ProfileScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = profileData.username,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                }
+            )
+        },
+        content = {
+            LazyColumn(
+                contentPadding = PaddingValues(16.dp),
+                content = {
+                    item {
+                        Column {
+                            Row(Modifier.height(IntrinsicSize.Min)) {
+                                Image(
+                                    modifier = Modifier
+                                        .size(size = 80.dp)
+                                        .clip(shape = CircleShape),
+                                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                                    contentDescription = null,
+                                )
+                                Spacer(modifier = Modifier.width(8.dp))
+                                FollowMeter(
+                                    modifier = Modifier.weight(1f),
+                                    count = "520",
+                                    label = "Posts"
+                                )
+                                FollowMeter(
+                                    modifier = Modifier.weight(1f),
+                                    count = "16.2M",
+                                    label = "Followers"
+                                )
+                                FollowMeter(
+                                    modifier = Modifier.weight(1f),
+                                    count = "1",
+                                    label = "Following"
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = "Prachan Ghale",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Black
+                            )
+                            Text(text = "Programmer", fontSize = 14.sp)
+                        }
+                    }
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun FollowMeter(
+    modifier: Modifier,
+    count: String? = "2",
+    label: String = "Posts"
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .clickable {
+
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = count ?: "0",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Text(
+            text = label,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Light
+        )
     }
 }
---
+++ dummy-data/build/generated/source/buildConfig/debug/app/prachang/dummy_data/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.prachang.dummy_data;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "app.prachang.dummy_data";
+  public static final String BUILD_TYPE = "debug";
+}
