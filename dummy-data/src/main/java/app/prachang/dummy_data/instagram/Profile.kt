5e1ee92df2b533a28ef09678fbf82363e75342fe
ghaleprachan <ghale.prachan@gmail.com>
2022-02-01 21:24:53 +0545

    [Add] adds model to show posts

---
+++ dummy-data/src/main/java/app/prachang/dummy_data/instagram/Profile.kt
@@ -0,0 +1,43 @@
+package app.prachang.dummy_data.instagram
+
+data class Profile(
+    val userId: String,
+    val name: String,
+    val username: String,
+    val image: String? = null,
+    val followerCount: Int = 0,
+    val followingCount: Int = 0,
+    val description: String? = null,
+    val link: String? = null
+)
+
+data class SavedStory(
+    val storyId: String,
+    val image: String,
+    val date: String?,
+    val label: String? = null,
+    val filterName: String? = null,
+    val tags: List<String> = listOf("@testuser", "@cr7", "@messi")
+)
+
+data class Post(
+    val postId: String,
+    val username: String,
+    val userImage: String,
+    val date: String,
+    val postImage: List<String>,
+    val likes: Int = 0,
+    val lastLikeBy: List<String> = emptyList(),
+    val description: String? = null,
+    val commentCount: Int? = 0,
+)
+
+data class PostComment(
+    val commentId: String,
+    val postId: String?,
+    val comment: String,
+    val username: String,
+    val date: String,
+    val commentLikes: Int = 0,
+    val userImage: String? = null,
+)
