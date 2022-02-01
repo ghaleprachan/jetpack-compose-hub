056b87377fb64028dcf4597f4d218074b1c620cc
ghaleprachan <ghale.prachan@gmail.com>
2022-02-01 09:20:15 +0545

    [Include] include base project on parent project

--- android-common/src/main/java/app/prachang/android_common/base/BaseActivity.kt
+++ android-common/src/main/java/app/prachang/android_common/base/BaseActivity.kt
@@ -2,9 +2,11 @@ package app.prachang.android_common.base
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import app.prachang.android_common.extensions.hideKeyboard
 
 abstract class BaseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideKeyboard()
     }
 }
