db93c267391db8cce02ed0edaf173deccea49054
ghaleprachan <ghale.prachan@gmail.com>
2022-02-01 21:27:23 +0545

    [Add] adds profile data for testing

---
+++ dummy-data/src/main/java/app/prachang/dummy_data/instagram/InstaData.kt
@@ -0,0 +1,14 @@
+package app.prachang.dummy_data.instagram
+
+import java.util.*
+
+val profileData = Profile(
+    userId = UUID.randomUUID().toString(),
+    name = "Prachan Ghale",
+    username = "g_prachan",
+    image = null,
+    followerCount = 233,
+    followingCount = 325,
+    description = null,
+    link = null
+)
