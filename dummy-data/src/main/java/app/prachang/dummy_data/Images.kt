6361d4f60f99f0f52fc41193625db49544fb64a6
ghaleprachan <ghale.prachan@gmail.com>
2022-02-02 22:06:47 +0545

    [Add] adds insta dummy images and dummy posts.

---
+++ dummy-data/src/main/java/app/prachang/dummy_data/Images.kt
@@ -0,0 +1,34 @@
+package app.prachang.dummy_data
+
+const val image1 = "https://imgk.timesnownews.com/story/ROnaldo-Insta-ap.jpg?tr=w-1200,h-900"
+const val image2 =
+    "https://as01.epimg.net/tikitakas/imagenes/2020/01/29/portada/1580317891_991006_1580318291_noticia_normal.jpg"
+const val image3 =
+    "https://site-cdn.givemesport.com/images/20/06/04/5a89834fb2569324a7da9feab388ecde/960.jpg"
+const val image4 = "https://i.insider.com/60f566fa7095a8001862e40c?width=700"
+const val image5 =
+    "https://imageio.forbes.com/specials-images/imageserve/1202979305/0x0.jpg?format=jpg&width=1200&fit=bounds"
+const val image6 =
+    "https://www.thesun.co.uk/wp-content/uploads/2020/05/SPORT-PREVIEW-Messi-Instagram.jpg"
+const val image7 =
+    "https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/se00rfzlrn0uxmrp_1591345258.jpeg"
+const val image8 = "https://i.pinimg.com/736x/c7/65/16/c7651615046a0c0aa5267f5f8f7866f8.jpg"
+const val image9 =
+    "https://cdn.wionews.com/sites/default/files/styles/story_page/public/2020/06/05/143417-untitled-design-54.jpg"
+const val image10 =
+    "https://c.ndtvimg.com/2021-06/22r02k5o_cristiano-ronaldo-afp_625x300_16_June_21.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675"
+const val image11 = "https://images.indianexpress.com/2021/09/Ronaldo-1.jpg"
+const val image12 = "https://m.media-amazon.com/images/I/61pO4fzVDQL._SL1279_.jpg"
+const val image13 =
+    "https://thesportsgrail.com/wp-content/uploads/2021/08/ronaldo-stock-images-1-1.jpg"
+const val image14 =
+    "https://football-italia.net/wp-content/uploads/2021/11/Cristiano-Ronaldo-playing-for-Juventus-at-Manchester-United.jpg"
+const val image15 =
+    "https://phantom-marca.unidadeditorial.es/3bbcc300c540a19e5eb71301bab43604/resize/1320/f/jpg/assets/multimedia/imagenes/2021/03/10/16153336358418.jpg"
+const val image16 = "https://i.ytimg.com/vi/qJqdJCj5u_4/maxresdefault.jpg"
+const val image17 =
+    "https://www.sportsmax.tv/media/k2/items/cache/88f6337eb391c460cd6bcfd6845ec104_XL.jpg"
+const val image18 =
+    "https://i2.wp.com/www.sportsbignews.com/wp-content/uploads/2019/08/New-Project-13.jpg?fit=640%2C460&ssl=1"
+const val image19 = "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202110/cristiano_ronaldo_reuters_1200x768.png?Bc.RmVr9LjS0Cg2CBwxb0cDlSLGp5zi0&size=770:433"
+const val image20 = "https://images.firstpost.com/wp-content/uploads/2012/06/ronaldo_5.jpg"
