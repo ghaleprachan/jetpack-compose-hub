94fd32f583e2acd93c4bfa77c2ecb94e12488923
ghaleprachan <ghale.prachan@gmail.com>
2022-02-02 22:08:59 +0545

    [Add] adds posts view

--- app/src/main/java/app/prachang/composehub/MainActivity.kt
+++ app/src/main/java/app/prachang/composehub/MainActivity.kt
@@ -1,7 +1,6 @@
 package app.prachang.composehub
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -13,21 +12,23 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import app.prachang.dummy_data.instagram.myPosts
 import app.prachang.dummy_data.instagram.profileData
 import app.prachang.theme.ComposeHubTheme
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -119,14 +120,33 @@ internal fun ProfileScreen() {
 
                     }
                 }
-                items((1..25).toList()) {
-                    ListItem {
-                        Text(text = "Test Title $it")
+                val paddingValues = PaddingValues(top = 2.dp, start = 2.dp)
+                val endPaddingValues = PaddingValues(start = 2.dp, end = 2.dp, top = 2.dp)
+                items(myPosts.windowed(3, 3, true)) { subList ->
+                    Row {
+                        subList.forEachIndexed { index, post ->
+                            val painter = rememberImagePainter(
+                                data = post.postImage[0],
+                                builder = {
+                                    transformations(RoundedCornersTransformation())
+                                }
+                            )
+                            Image(
+                                painter = painter,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .padding(if (subList.lastIndex == index) endPaddingValues else paddingValues)
+                                    .weight(1f)
+                                    .aspectRatio(1f),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
                     }
                 }
             }
         )
     }
+    // val painter = rememberImagePainter("")
 }
 
 @Composable
