f8e1a74f0470aafddc992b7725260a27ac3f92e9
ghaleprachan <ghale.prachan@gmail.com>
2022-02-08 21:24:27 +0545

    [Add] adds profile screen on instagram profile

--- instagram-clone/src/main/java/app/prachang/instagram_clone/InstaProfileScreen.kt
+++ instagram-clone/src/main/java/app/prachang/instagram_clone/InstaProfileScreen.kt
@@ -31,3 +31,150 @@ import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
 
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ProfileScreenPreview() {
+    ComposeHubTheme {
+        ProfileScreen()
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
+@Composable
+internal fun ProfileScreen() {
+    val elevation = remember {
+        mutableStateOf(0.dp)
+    }
+    val scrollState = rememberLazyListState()
+    if (scrollState.firstVisibleItemScrollOffset > 0) {
+        elevation.value = 8.dp
+    } else {
+        elevation.value = 0.dp
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                elevation = elevation.value,
+                title = {
+                    Text(
+                        text = profileData.username,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                    )
+                }
+            )
+        }
+    ) {
+        LazyColumn(
+            state = scrollState,
+            content = {
+                item {
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Row(Modifier.height(IntrinsicSize.Min)) {
+                            val painter = rememberImagePainter(
+                                data = image2,
+                                builder = {
+                                    transformations(CircleCropTransformation())
+                                }
+                            )
+                            Image(
+                                modifier = Modifier
+                                    .size(size = 85.dp),
+                                painter = painter,
+                                contentDescription = null,
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            FollowMeter(
+                                modifier = Modifier.weight(1f),
+                                count = profileData.totalPosts,
+                                label = "Posts"
+                            )
+                            FollowMeter(
+                                modifier = Modifier.weight(1f),
+                                count = profileData.followerCount,
+                                label = "Followers"
+                            )
+                            FollowMeter(
+                                modifier = Modifier.weight(1f),
+                                count = profileData.followingCount,
+                                label = "Following"
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = profileData.name,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Black
+                        )
+                        Text(text = "Athlete", fontSize = 14.sp)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+
+                stickyHeader {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(70.dp)
+                            .background(Color.Gray)
+                    ) {
+
+                    }
+                }
+                val firstPadding = PaddingValues(top = 2.dp, start = 2.dp)
+                val lastPadding = PaddingValues(start = 2.dp, end = 2.dp, top = 2.dp)
+
+                items(myPosts.windowed(3, 3, true)) { subList ->
+                    Row {
+                        subList.forEachIndexed { index, post ->
+                            val painter = rememberImagePainter(
+                                data = post.postImage[0],
+                                builder = {
+                                    transformations(RoundedCornersTransformation())
+                                }
+                            )
+                            Image(
+                                painter = painter,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .padding(if (subList.lastIndex == index) lastPadding else firstPadding)
+                                    .weight(1f)
+                                    .aspectRatio(1f),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun FollowMeter(
+    modifier: Modifier,
+    count: String? = "2",
+    label: String = "Posts"
+) {
+    Column(
+        modifier = modifier
+            .fillMaxHeight()
+            .clickable {
+
+            },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = count ?: "0",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Text(
+            text = label,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Light
+        )
+    }
+}
