0c21b082a2a631aeb8f19376dcc58b6cc6ef807e
ghaleprachan <ghale.prachan@gmail.com>
2022-02-10 22:19:30 +0545

    [Add] adds post details profile section

--- instagram-clone/src/main/java/app/prachang/instagram_clone/homescreen/InstaHomeScreen.kt
+++ instagram-clone/src/main/java/app/prachang/instagram_clone/homescreen/InstaHomeScreen.kt
@@ -1,41 +1,18 @@
 package app.prachang.instagram_clone
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Message
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import app.prachang.common_compose_ui.extensions.Height
-import app.prachang.dummy_data.instagram.Post
-import app.prachang.dummy_data.instagram.kotlinIcon
 import app.prachang.dummy_data.instagram.myPosts
-import app.prachang.instagram_clone.homescreen.MyStoryItem
+import app.prachang.instagram_clone.homescreen.PostItem
 import app.prachang.instagram_clone.homescreen.StoryContent
-import app.prachang.instagram_clone.homescreen.StoryItem
-import coil.compose.rememberImagePainter
 
 @Preview(showSystemUi = true)
 @Composable
@@ -80,10 +57,8 @@ fun InstaHomeScreen() {
                 item {
                     StoryContent()
                 }
-                items((1..100).toList()) {
-                    ListItem {
-                        Text(text = "Title $it")
-                    }
+                items(myPosts) {
+                    PostItem(post = it)
                 }
             }
         )
--- instagram-clone/src/main/java/app/prachang/instagram_clone/homescreen/PostsContent.kt
+++ instagram-clone/src/main/java/app/prachang/instagram_clone/homescreen/PostsContent.kt
@@ -1,2 +1,70 @@
 package app.prachang.instagram_clone.homescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import app.prachang.common_compose_ui.extensions.Height
+import app.prachang.common_compose_ui.extensions.Width
+import app.prachang.dummy_data.instagram.Post
+import coil.compose.rememberImagePainter
+
+/**
+ *  Here update post image using pager and other UI improvements
+ *
+ *  Also add previews
+ *  todo*/
+@Composable
+fun PostItem(post: Post) {
+    val profilePainter = rememberImagePainter(data = post.userImage)
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = profilePainter,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(shape = CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Width(width = 14)
+            Text(
+                text = post.username,
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold,
+                ),
+            )
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.End)
+            ) {
+                Icon(
+                    Icons.Default.MoreVert,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
