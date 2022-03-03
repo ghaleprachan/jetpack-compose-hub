d3989f376c2914ea2e5396f9e26024330ba7dbb3
ghaleprachan <ghale.prachan@gmail.com>
2022-03-03 14:30:28 +0545

    [Add] adds sample route on Routes.kt class

--- gmail-clone/src/main/java/app/prachang/gmail_clone/Routes.kt
+++ gmail-clone/src/main/java/app/prachang/gmail_clone/Routes.kt
@@ -1,6 +1,21 @@
 package app.prachang.gmail_clone
 
-object GmailRoutes {
+internal object GmailRoutes {
     const val HomeScreen = "route-home"
     const val SearchScreen = "route-search"
+    const val EmailDetail = "route-email-details/{email_id}"
+}
+
+// This is better way to use route when having
+// parameter to pass or else just use enums to be more specific or else object
+internal sealed class Routes(
+    val route: String
+) {
+    object HomeScreen : Routes(GmailRoutes.HomeScreen)
+    object SearchScreen : Routes(GmailRoutes.SearchScreen)
+    object EmailDetail : Routes(GmailRoutes.EmailDetail) {
+        fun getRoute(emailId: String): String {
+            return "$route/$emailId"
+        }
+    }
 }
--- gmail-clone/src/main/java/app/prachang/gmail_clone/repository.kt
+++
@@ -1,2 +0,0 @@
-package app.prachang.gmail_clone
-
