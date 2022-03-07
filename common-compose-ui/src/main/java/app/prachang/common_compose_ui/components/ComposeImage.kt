7f846b53a679b257fc3ab3c5cbafd9d2fc002b4b
ghaleprachan <ghale.prachan@gmail.com>
2022-03-07 15:47:20 +0545

    [Add] adds Readme.md

--- common-compose-ui/src/main/java/app/prachang/common_compose_ui/components/ComposeImage.kt
+++ common-compose-ui/src/main/java/app/prachang/common_compose_ui/components/ComposeImage.kt
@@ -1,21 +1,17 @@
 package app.prachang.common_compose_ui.components
 
-import android.provider.CalendarContract
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import app.prachang.common_compose_ui.R
