8332775d380cbabcf22149be5a78356b3bb974b7
ghaleprachan <ghale.prachan@gmail.com>
2022-03-08 16:12:17 +0545

    [Refactor] chages meet UI

--- gmail-clone/src/main/java/app/prachang/gmail_clone/home/MeetScreen.kt
+++ gmail-clone/src/main/java/app/prachang/gmail_clone/home/MeetScreen.kt
@@ -1,37 +1,21 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
 package app.prachang.gmail_clone.home
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import app.prachang.common_compose_ui.components.CircleImage
 import app.prachang.common_compose_ui.components.ProgressScreen
-import app.prachang.dummy_data.instagram.kotlinIcon
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MeetScreen() {
     Scaffold(
         topBar = {
-            MediumTopAppBar(
+            CenterAlignedTopAppBar(
                 title = { Text(text = "Meet") },
-                navigationIcon = {
-                    IconButton(onClick = { }) {
-                        Icon(imageVector = Icons.Default.Menu, contentDescription = null)
-                    }
-                },
-                actions = {
-                    CircleImage(
-                        url = kotlinIcon,
-                        modifier = Modifier
-                            .padding(end = 12.dp)
-                            .size(30.dp),
-                    )
-                },
             )
         },
     ) {
