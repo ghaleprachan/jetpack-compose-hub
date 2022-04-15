4d7c45decb3efb72372692369f43005d1f79b31f
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2022-04-15 22:48:36 +0545

    [Add] adds facebook module

--- app/src/main/java/app/prachang/composehub/AppConstants.kt
+++ app/src/main/java/app/prachang/composehub/AppConstants.kt
@@ -1,11 +1,7 @@
 package app.prachang.composehub
 
-class AppConstants {
-    companion object{
-        object Colums {
-            const val ONE = 1
-            const val TWO = 2
-            const val THREE = 3
-        }
-    }
+object AppConstants {
+    const val ONE = 1
+    const val TWO = 2
+    const val THREE = 2
 }
---
+++ facebook-clone/src/main/java/com/example/facebook_clone/FacebookHomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.facebook_clone
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FacebookHomeScree() {
+    Text(text = "Facebook work on progress")
+}
