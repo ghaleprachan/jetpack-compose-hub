8329021cad431394e2fbf7b9edec2a327c4cbad1
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2022-07-04 22:03:51 +0545

    [Add] adds navigation for facebook clone app

--- facebook-clone/src/main/java/com/example/facebook_clone/FacebookHomeScreen.kt
+++ facebook-clone/src/main/java/com/example/facebook_clone/FacebookHomeScreen.kt
@@ -1,9 +1,15 @@
 package com.example.facebook_clone
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FacebookHomeScree() {
-    Text(text = "Facebook work on progress")
+fun FacebookHomeScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Facebook work on progress")
+    }
 }
