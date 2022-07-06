ae88f0d8fb4d0c19155b7e4df3a615c644ca3b6d
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2022-07-06 16:24:15 +0545

    [Add] adds facebook tab bar

--- facebook-clone/src/main/java/com/example/facebook_clone/FacebookHomeScreen.kt
+++ facebook-clone/src/main/java/com/example/facebook_clone/FacebookHomeScreen.kt
@@ -1,15 +1,90 @@
 package com.example.facebook_clone
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
 @Composable
 fun FacebookHomeScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Facebook work on progress")
+    FacebookHomeContent()
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun FacebookHomeContent() {
+    val tabs = BottomNavItems.values()
+    val coroutineScope = rememberCoroutineScope()
+    val pagerState = rememberPagerState()
+
+    Column {
+        AnimatedVisibility(visible = pagerState.currentPage == BottomNavItems.Home.ordinal) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = "Facebook",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp),
+                )
+            }
+        }
+
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions),
+                    color = Color.Blue,
+                )
+            },
+            backgroundColor = Color.White,
+        ) {
+            tabs.forEachIndexed { index, tab ->
+                Tab(
+                    selected = pagerState.currentPage == index,
+                    selectedContentColor = Color.Blue,
+                    unselectedContentColor = Color.DarkGray,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.scrollToPage(index)
+                        }
+                    },
+                    // text = { Text(title) },
+                    icon = {
+                        Icon(
+                            modifier = Modifier.size(22.dp),
+                            painter = painterResource(id = if (pagerState.currentPage == index) tab.selectedIcon else tab.unSelectedIcon),
+                            contentDescription = null,
+                        )
+                    }
+                )
+            }
+        }
+        HorizontalPager(count = tabs.size, state = pagerState) { page: Int ->
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Current Page $page")
+            }
+        }
     }
 }
+
