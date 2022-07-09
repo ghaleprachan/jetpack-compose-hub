de408505591d460de1c6291add81da02be9b9abc
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2022-07-09 12:40:48 +0545

    [Add] adds whats on your mind item

---
+++ facebook-clone/src/main/java/com/example/facebook_clone/FacebookConstants.kt
@@ -0,0 +1,5 @@
+package com.example.facebook_clone
+
+object FacebookConstants {
+    const val ProfileImage = "https://assets.entrepreneur.com/content/3x2/2000/1651673015-shutterstock-2098527055.jpg"
+}
