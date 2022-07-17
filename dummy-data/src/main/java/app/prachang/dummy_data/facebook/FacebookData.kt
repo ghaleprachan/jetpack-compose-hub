199511a66c191d66c7e20cf5a642d4d45ea51d40
Prachan Ghale <70263211+ghaleprachan@users.noreply.github.com>
2022-07-17 20:22:26 +0545

    [Add] adds dummy data model

---
+++ dummy-data/src/main/java/app/prachang/dummy_data/facebook/FacebookData.kt
@@ -0,0 +1,10 @@
+package app.prachang.dummy_data.facebook
+
+data class FacebookDataModel(
+    val name: String,
+    val profile: String,
+    val time: String,
+    val likes: String,
+    val comments: String,
+    val shares: String,
+)
